c9187e02e752678baebd250a2323f0495fea2713
Marco <marco.wettstein@gmail.com>
2021-12-06 12:46:33 +0100

    Day 06 - Optimised to only track population count

--- 06/main.go
+++ 06/main.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-func parse_file(file_name string) (fishies []int, err error) {
+func parse_population(file_name string) (population []int, err error) {
 	file, err := os.Open(file_name)
 	if err != nil {
 		return nil, err
@@ -20,43 +20,41 @@ func parse_file(file_name string) (fishies []int, err error) {
 	scanner.Scan()
 	line := scanner.Text()
 	splitted := strings.Split(line, ",")
-	fishies = make([]int, 0, len(splitted))
+	population = make([]int, 9)
 	for _, str := range splitted {
-		number, _ := strconv.Atoi(str)
-		fishies = append(fishies, number)
+		age, _ := strconv.Atoi(str)
+		population[age]++
 	}
 
-	return fishies, nil
+	return population, nil
 }
 
-func simulate_population(fishies []int, days int) (total_fishies int) {
-	newFishies := make([]int, 0)
+func simulate_population(population []int, days int) (total_fishies int64) {
+
 	for i := 0; i < days; i++ {
-		for j := 0; j < len(fishies); j++ {
-			fishies[j]--
-
-			if fishies[j] == -1 {
-				fishies[j] = 6
-				newFishies = append(newFishies, 8)
-			}
+		new_fishes := population[0]
+		for j := 1; j < len(population); j++ {
+			population[j-1] = population[j]
 		}
-
-		if len(newFishies) > 0 {
-			fishies = append(fishies, newFishies...)
-			newFishies = make([]int, 0)
-		}
+		population[6] = new_fishes + population[6] // sum of fishes with counter < 0 and those that were counted down from 7
+		population[8] = new_fishes
 	}
 
-	return len(fishies)
+	var total int64 = 0
+	for _, count := range population {
+		total += (int64)(count)
+	}
+
+	return total
 }
 
 func main() {
-	input_fishies, _ := parse_file("testinput.txt")
-	fishies := input_fishies
-	fish_count := simulate_population(fishies, 80)
+	file := "input.txt"
+	population, _ := parse_population(file)
+	fish_count := simulate_population(population, 80)
 	fmt.Printf("Solution Part 1: %d\n", fish_count)
 
-	fishies = input_fishies
-	fish_count = simulate_population(fishies, 256)
+	population, _ = parse_population(file)
+	fish_count = simulate_population(population, 256)
 	fmt.Printf("Solution Part 2: %d\n", fish_count)
 }
